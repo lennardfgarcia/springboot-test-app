3cd825e83045e1073c32b21982b274dfafc3766f
Lennard Garcia <lennardfgarcia@gmail.com>
2025-07-27 18:06:36 +0200

    - added the Publisher entity

--- src/main/java/com/example/demo/bootstrap/BootstrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootstrapData.java
@@ -2,23 +2,28 @@ package com.example.demo.bootstrap;
 
 import com.example.demo.domain.Author;
 import com.example.demo.domain.Book;
+import com.example.demo.domain.Publisher;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.BookRepository;
+import com.example.demo.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Lennard for the Spring Boot 3 training course
  */
+
 @Component
 public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -33,7 +38,7 @@ public class BootstrapData implements CommandLineRunner {
         hygge.setTitle("The Little Book of Hygge");
         hygge.setIsbn("123456789");
 
-        // Create repository
+        // Persist the information to the respective DBs
         Author miekSaved = authorRepository.save(miek);
         Book hyggeSaved = bookRepository.save(hygge);
 
@@ -46,7 +51,7 @@ public class BootstrapData implements CommandLineRunner {
         dm.setTitle("Dragon Masters");
         dm.setIsbn("9876543321");
 
-        // Create repository
+        // Persist the information to the respective DBs
         Author westSaved = authorRepository.save(west);
         Book dmSaved = bookRepository.save(dm);
 
@@ -64,5 +69,12 @@ public class BootstrapData implements CommandLineRunner {
 
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+
+        Publisher happy = new Publisher();
+        happy.setPublisherName("Happy Publisher");
+        happy.setZipCode("12345");
+        publisherRepository.save(happy);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
